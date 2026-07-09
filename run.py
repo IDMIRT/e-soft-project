6be3f963285c3d907c2a95bc064bd6683db350e0
Дмитрий Иванов <ida78@inbox.ru>
2026-07-10 01:00:48 +1100

    Доработки работы с БД(вставка в таблицу UploadedFiles)

---
+++ run.py
@@ -0,0 +1,23 @@
+from app.db import engine, create_tables
+# from models import UploadedFiles, ResultAnalysis
+from flask import Flask
+from app.api import start_page, test_page_number
+
+errors = False
+
+try:
+    with engine.connect() as conn:        
+        create_tables()
+except Exception as ex:
+    errors=True
+    print(f"Ошибка подключения к БД")
+
+if not(errors):
+    app = Flask(__name__)
+    # app.add_url_rule('/','start',start_page)
+    # app.add_url_rule('/page/<int:number_page>','number_page',test_page_number)
+
+
+
+
+    app.run(debug=False)
